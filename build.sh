193003fe626d2b91f0799a1c1754fe20c8f62740
chris13524 <chris@hichris.com>
2018-09-22 16:25:17 -0400

    Deleted unnecessary file.

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-version="$1"
-if [ -z "$version" ]; then
-	echo "Missing version parameter."
-	exit 1
-fi
-
-docker build --build-arg VERSION=$version -t chris13524/angular-cli:$version .
